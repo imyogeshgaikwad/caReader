d3c9f95d5b8d930460180a3e0533da810011587b
BEAST <imyogeshgaikwad@gmail.com>
2025-11-03 18:44:45 +0000

    Done with all routes

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app =  express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Listing = require("./models/listing.js");
+const methodOverride = require("method-override");
+
+
 
 const MONGO_URL ="mongodb://127.0.0.1:27017/temp"
 main().then(()=>{
@@ -17,6 +20,8 @@ async function main(){
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+
 
 app.get("/",(req,res)=>{
     res.send("Hi I am root!")
@@ -49,7 +54,27 @@ app.post("/listings", async(req,res)=>{
     
 })
 
+//edit route
+app.get("/listings/:id/edit", async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+});
 
+
+//Update route
+app.put("/listings/:id", async (req, res) => {
+    const { id } = req.params;
+    await Listing.findByIdAndUpdate(id, req.body.listing, { runValidators: true });
+    res.redirect(`/listings/${id}`);
+});
+
+//DELETE ROUTE
+app.delete("/listings/:id",async(req,res)=>{
+      const { id } = req.params;
+     let deletedListing = await Listing.findByIdAndDelete(id);
+     res.redirect("/listings")
+});
 app.listen(8080,()=>{
      console.log("server is listening on port 8080")
 });
