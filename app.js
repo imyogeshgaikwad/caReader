1c52c3c81ae92551c41b6f48932aa0e9ec155aea
BEAST <imyogeshgaikwad@gmail.com>
2025-11-03 17:23:52 +0000

    Update in the init files and app.js
    added ejs files and some routes in the app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app =  express();
 const mongoose = require("mongoose");
-const Listings = require("./models/listing.js");
 const path = require("path");
+const Listing = require("./models/listing.js");
 
 const MONGO_URL ="mongodb://127.0.0.1:27017/temp"
 main().then(()=>{
@@ -14,15 +14,41 @@ async function main(){
     await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
+
 app.get("/",(req,res)=>{
     res.send("Hi I am root!")
 });
 
-
+//index route
 app.get("/listings",async(req,res)=>{
-   const allListings =  await Listings.find({});
-    res.render("index.ejs",{allListings});
+ const allListings = await Listing.find({});
+    res.render("listings/index.ejs",{allListings});
 });
+
+//New Route
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs")
+})
+
+//Show Route
+app.get("/listings/:id",async(req,res)=>{
+    let {id}= req.params;
+   const listing = await Listing.findById(id);
+   res.render("listings/show.ejs",{listing})
+    
+});
+
+//Create Route
+app.post("/listings", async(req,res)=>{
+    const newListing = new Listing (req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+    
+})
+
 
 app.listen(8080,()=>{
      console.log("server is listening on port 8080")
