ae55c12c928dfd3738580c2c2cd3ba08a1d9c050
BEAST <imyogeshgaikwad@gmail.com>
2025-11-12 12:01:57 +0000

    Redirecting Url Implementation

--- routes/user.js
+++ routes/user.js
@@ -3,57 +3,56 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
+// SIGNUP
 router.get("/signup", (req, res) => {
-    res.render("users/signup.ejs");
+  res.render("users/signup.ejs");
 });
 
 router.post("/signup", wrapAsync(async (req, res, next) => {
-    try {
-        let { username, email, password } = req.body;
-        const newUser = new User({ email, username });
-        const registeredUser = await User.register(newUser, password);
-        console.log(registeredUser);
-        
-        // Auto-login after successful registration
-        req.login(registeredUser, (err) => {
-            if (err) {
-                return next(err);
-            }
-            req.flash("success", "Welcome to CAReader!");
-            res.redirect("/listings");
-        });
-    } catch (e) {
-        req.flash("error", e.message);
-        res.redirect("/signup");
-    }
+  try {
+    const { username, email, password } = req.body;
+    const newUser = new User({ email, username });
+    const registeredUser = await User.register(newUser, password);
+    req.login(registeredUser, (err) => {
+      if (err) return next(err);
+      req.flash("success", "Welcome to CAReader!");
+      res.redirect("/listings");
+    });
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/signup");
+  }
 }));
 
-//login
-
+// LOGIN
 router.get("/login", (req, res) => {
-    res.render("users/login.ejs");
+  res.render("users/login.ejs");
 });
 
-router.post("/login", 
-    passport.authenticate("local", {
-        failureRedirect: "/login",
-        failureFlash: true
-    }), 
-    async (req, res) => {
-        req.flash("success", "Welcome back to CAReader!");
-        res.redirect("/listings");
-    }
+router.post(
+  "/login",
+  saveRedirectUrl,
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  (req, res) => {
+    req.flash("success", "Welcome back to CAReader!");
+    const redirectUrl = res.locals.redirectUrl || "/listings";
+    delete req.session.redirectUrl;
+    res.redirect(redirectUrl);
+  }
 );
 
+// LOGOUT
 router.get("/logout", (req, res, next) => {
-    req.logout((err) => {
-        if (err) {
-            return next(err);
-        }
-        req.flash("success", "You have been logged out successfully!");
-        res.redirect("/listings");
-    });
+  req.logout((err) => {
+    if (err) return next(err);
+    req.flash("success", "You have been logged out successfully!");
+    res.redirect("/listings");
+  });
 });
 
 module.exports = router;
